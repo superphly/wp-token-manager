48b1644d6316a9b32eab4572e90139b400881003
Cody Marx Bailey <superphly@gmail.com>
2019-05-23 18:45:29 -0500

    small tweaks, etherscan link, title update

--- interface.php
+++ interface.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-  <h1 class="wp-heading-inline">721 Management</h1>
+  <h1 class="wp-heading-inline">Crypto Asset Management</h1>
   <hr class="wp-header-end">
   <table class="widefat fixed" cellspacing="0" style="width: auto;">
     <thead>
@@ -17,7 +17,7 @@
       <td>Johnny Johnson (Gold/Blue)</td>
       <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
       <td>QmUQcSjQx2bg4cSe2rUZyQi6F8QtJFJb74fWL7D784UWf9</td>
-      <td><a href="#">View</a></td>
+      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=0">View</a></td>
       <td><a href="#" class="button button-primary">Transfer</a></td>
     </tr>
     <tr>
@@ -25,7 +25,7 @@
       <td>Timmy Timmerson (Silver/Purple)</td>
       <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
       <td>QmdytmR4wULMd3SLo6ePF4s3WcRHWcpnJZ7bHhoj3QB13v</td>
-      <td><a href="#">View</a></td>
+      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=1">View</a></td>
       <td><a href="#" class="button button-primary">Transfer</a></td>
     </tr>
     <tr>
@@ -33,8 +33,9 @@
       <td>Richard Rickards (Red/Black)</td>
       <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
       <td>QmSFxnK675wQ9Kc1uqWKyJUaNxvSc2BP5DbXCD3x93oq61</td>
-      <td><a href="#">View</a></td>
+      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=2">View</a></td>
       <td><a href="#" class="button button-primary">Transfer</a></td>
     </tr>
   </table>
+  <a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a">Token View on Etherscan</a>
 </div>
