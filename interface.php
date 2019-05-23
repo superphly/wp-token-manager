713df8a86f7e994a66fd099b2e1a2d5814755e63
Cody Marx Bailey <superphly@gmail.com>
2019-05-23 17:20:06 -0500

    div wrapper

--- interface.php
+++ interface.php
@@ -1,38 +1,40 @@
-<h1 class="wp-heading-inline">721 Management Forever</h1>
-<hr class="wp-header-end">
-<table class="widefat fixed" cellspacing="0">
-  <thead>
+<div class="wrap">
+  <h1 class="wp-heading-inline">721 Management Forever</h1>
+  <hr class="wp-header-end">
+  <table class="widefat fixed" cellspacing="0">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Asset Name</th>
+        <th>Owner (Address)</th>
+        <th>Auction Active</th>
+        <th>Etherscan</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
     <tr>
-      <th>ID</th>
-      <th>Asset Name</th>
-      <th>Owner (Address)</th>
-      <th>Auction Active</th>
-      <th>Etherscan</th>
-      <th>Actions</th>
+      <td>0</td>
+      <td>Johnny Johnson (Gold/Blue)</td>
+      <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
+      <td>False</td>
+      <td>View</td>
+      <td>Transfer</td>
     </tr>
-  </thead>
-  <tr>
-    <td>0</td>
-    <td>Johnny Johnson (Gold/Blue)</td>
-    <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
-    <td>False</td>
-    <td>View</td>
-    <td>Transfer</td>
-  </tr>
-  <tr>
-    <td>0</td>
-    <td>Timmy Timmerson (Silver/Purple)</td>
-    <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
-    <td>False</td>
-    <td>View</td>
-    <td>Transfer</td>
-  </tr>
-  <tr>
-    <td>0</td>
-    <td>Richard Rickards (Red/Black)</td>
-    <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
-    <td>False</td>
-    <td>View</td>
-    <td>Transfer</td>
-  </tr>
-</table>
+    <tr>
+      <td>0</td>
+      <td>Timmy Timmerson (Silver/Purple)</td>
+      <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
+      <td>False</td>
+      <td>View</td>
+      <td>Transfer</td>
+    </tr>
+    <tr>
+      <td>0</td>
+      <td>Richard Rickards (Red/Black)</td>
+      <td>0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</td>
+      <td>False</td>
+      <td>View</td>
+      <td>Transfer</td>
+    </tr>
+  </table>
+</div>
