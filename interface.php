d4eb64d19f2f8fe32685d770c68919aab47327eb
Cody Marx Bailey <superphly@gmail.com>
2019-05-23 23:28:28 -0500

    render rows

--- interface.php
+++ interface.php
@@ -12,37 +12,47 @@
         <th>Actions</th>
       </tr>
     </thead>
-    <tr>
-      <td>0</td>
-      <td>Johnny Johnson (Gold/Blue)</td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=0x7dfead29a1bbe27cfa38ba73f84872db40932844">0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</a></td>
-      <td><a href="https://ipfs.infura.io/ipfs/QmUQcSjQx2bg4cSe2rUZyQi6F8QtJFJb74fWL7D784UWf9">QmUQcSjQx2bg4cSe2rUZyQi6F8QtJFJb74fWL7D784UWf9</a></td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=0">View</a></td>
-      <td><a href="#" class="button button-primary">Transfer</a></td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Timmy Timmerson (Silver/Purple)</td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=0x7dfead29a1bbe27cfa38ba73f84872db40932844">0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</a></td>
-      <td><a href="https://ipfs.infura.io/ipfs/QmdytmR4wULMd3SLo6ePF4s3WcRHWcpnJZ7bHhoj3QB13v">QmdytmR4wULMd3SLo6ePF4s3WcRHWcpnJZ7bHhoj3QB13v</a></td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=1">View</a></td>
-      <td><a href="#" class="button button-primary">Transfer</a></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Richard Rickards (Red/Black)</td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=0x7dfead29a1bbe27cfa38ba73f84872db40932844">0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844</a></td>
-      <td><a href="https://ipfs.infura.io/ipfs/QmSFxnK675wQ9Kc1uqWKyJUaNxvSc2BP5DbXCD3x93oq61">QmSFxnK675wQ9Kc1uqWKyJUaNxvSc2BP5DbXCD3x93oq61</a></td>
-      <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a=2">View</a></td>
-      <td><a href="#" class="button button-primary">Transfer</a></td>
-    </tr>
+    <div class="holder"></div>
   </table>
   <br>
   <a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a">Token View on Etherscan</a>
 </div>
 
+<script id="row" type="text/x-jsrender">
+  <tr>
+    <td>{{:id}}</td>
+    <td>{{:name}}</td>
+    <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a={{:owner}}">{{:owner}}</a></td>
+    <td><a href="https://ipfs.infura.io/ipfs/{{:ipfs}}">{{:ipfs}}</a></td>
+    <td><a href="https://ropsten.etherscan.io/token/0xfed7020a24472aca24b1afa2f71a388c17f6634a?a={{:id}}">View</a></td>
+    <td><a href="#" class="button button-primary">Transfer</a></td>
+  </tr>
+</script>
+
 <script>
   jQuery(document).ready(function( $ ) {
-    $('.wp-heading-inline').hide();
+    var tokens = [
+      {
+        id: 0,
+        name: 'Johnny Johnson (Gold/Blue)',
+        owner: '0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844',
+        ipfs: 'QmSFxnK675wQ9Kc1uqWKyJUaNxvSc2BP5DbXCD3x93oq61'
+      },
+      {
+        id: 1,
+        name: 'Timmy Timmerson (Silver/Purple)',
+        owner: '0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844',
+        ipfs: 'QmdytmR4wULMd3SLo6ePF4s3WcRHWcpnJZ7bHhoj3QB13v'
+      },
+      {
+        id: 2,
+        name: 'Richard Rickards (Red/Black)',
+        owner: '0x7DfEAd29A1BBE27cFA38Ba73f84872DB40932844',
+        ipfs: 'QmSFxnK675wQ9Kc1uqWKyJUaNxvSc2BP5DbXCD3x93oq61'
+      },      
+    ]
+    var row = $.templates('#row');
+    var rows = row.render(tokens);
+    $('div.holder').replace(rows);
   });
 </script>
